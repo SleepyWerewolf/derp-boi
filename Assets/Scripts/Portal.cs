7d9d3013776b6c21a335664452950a307e7a321e
viettruong2194 <viettruong2194@gmail.com>
2015-04-15 20:23:57 -0700

    Finish last level

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -13,7 +13,7 @@ public class Portal : MonoBehaviour {
 
 	private float adjust = 1f;
 	private float localScaleX = 0f;
-	public float portalThrust = 250f;
+	public float portalThrust = 1000f;
 
 	// Use this for initialization
 	void Start () {
@@ -70,7 +70,7 @@ public class Portal : MonoBehaviour {
 			} else {
 				target.gameObject.transform.position = new Vector3(targetPortal.transform.position.x + (-targetPortal.transform.localScale.x * adjust), targetPortal.transform.position.y, 0);
 				target.gameObject.transform.localScale = new Vector3(localScaleX, 1, targetPortal.transform.localScale.z);
-				target.rigidbody2D.AddForce(new Vector2(localScaleX * portalThrust, 50f));
+				target.rigidbody2D.AddForce(new Vector2(localScaleX * portalThrust, 0));
 			}
 		}
 	}
